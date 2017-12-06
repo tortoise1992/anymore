7f2c29474490000637bef18c1074482a277d2e3e
ahui666 <772084015@qq.com>
2017-12-06 14:02:19 +0800

    一些小优化

--- ahuife/blog/models.py
+++ ahuife/blog/models.py
@@ -25,3 +25,6 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('blog:detail',kwargs={'pk':self.pk})
         # print(self.pk)
+
+    class Meta:
+        ordering=['-create_time']
